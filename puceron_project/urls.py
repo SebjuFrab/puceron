32d8ea08af92004717b53694b99075598ad37b04
SebjuFrab <s.julliard@agrobio-bretagne.org>
2026-04-02 13:23:04 +0200

    reénitialisation

--- puceron_project/urls.py
+++ puceron_project/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -27,7 +28,37 @@ urlpatterns = [
     path('cms/documents/', include(wagtaildocs_urls)),
     path('', include('scouting.urls')),
     path('api/', include('scouting.api_urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path(
+        'accounts/login/',
+        auth_views.LoginView.as_view(template_name='registration/login.html'),
+        name='login',
+    ),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path(
+        'accounts/password_reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='registration/password_reset_form.html',
+            email_template_name='registration/password_reset_email.html',
+            html_email_template_name='registration/password_reset_email_html.html',
+            subject_template_name='registration/password_reset_subject.txt',
+        ),
+        name='password_reset',
+    ),
+    path(
+        'accounts/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+        name='password_reset_done',
+    ),
+    path(
+        'accounts/reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
+        name='password_reset_confirm',
+    ),
+    path(
+        'accounts/reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+        name='password_reset_complete',
+    ),
 ]
 
 if settings.DEBUG:
